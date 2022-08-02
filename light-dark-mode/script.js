3fb8666523bb5ba12d3772208818bddff03c7c22
coach77777 <108881946+coach77777@users.noreply.github.com>
2022-08-02 05:19:38 -0500

    js projects

---
+++ light-dark-mode/script.js
@@ -0,0 +1,67 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+//Dark or light mode
+function imageMode(color) {
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+// Dark Mode Styles
+function darkMode(){
+    nav.style.backgroundColor = 'rgb(0 0 0 /50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 /50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    // Condensing Code DRY Code
+    //Remove add line and use replace instead of remove
+    //toggleIcon.children[1].classList.remove('fa-sun');
+    //toggleIcon.children[1].classList.add('fa-moon');
+    toggleIcon.children[1].classList.replace('fa-sun','fa-moon'); 
+    //change to function dark to color variable
+    //image1.src = "img/undraw_proud_coder_dark.svg";
+    //image2.src = "img/undraw_feeling_proud_dark.svg";
+    //image3.src = "img/undraw_conceptual_idea_dark.svg";
+    imageMode('dark');
+}
+
+// Light Mode Styles
+function lightMode(){
+    nav.style.backgroundColor = 'rgb(255 255 255 /50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 /50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
+    
+}
+
+// Switch Theme light to dark
+function switchTheme(event){
+    if(event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        darkMode();
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        lightMode();
+    }
+}
+
+// Event Listener
+toggleSwitch.addEventListener("change", switchTheme);
+
+// Check local storage for Theme
+const currentTheme = localStorage.getItem('theme');
+    if(currentTheme){
+        document.documentElement.setAttribute('data-theme', currentTheme);
+        if(currentTheme === 'dark'){
+            toggleSwitch.checked = true;
+            darkMode();
+        }
+    }
